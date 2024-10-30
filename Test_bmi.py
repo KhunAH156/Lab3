cddcca90556c06d1cb8f8e737be43a4afdb629d7
KhunAungHein <khunah.23@ichat.sp.edu.sg>
2024-10-30 22:33:56 +0800

    Implemented all unit tests

--- Test_bmi.py
+++ Test_bmi.py
@@ -7,3 +7,21 @@ def test_bmi_normal_weight():
     test_output=0
     result=bmi.calculate_bmi(input_height,input_weight)
     assert (result==test_output)
+
+def test_bmi_over_weight():
+    result=[]
+    input_weight=90
+    input_height=1.73
+    test_output=1
+    result=bmi.calculate_bmi(input_height,input_weight)
+    assert (result==test_output)
+
+def test_bmi_under_weight():
+    result=[]
+    input_weight=30
+    input_height=1.73
+    test_output=-1
+    result=bmi.calculate_bmi(input_height,input_weight)
+    assert (result==test_output)
+
+
